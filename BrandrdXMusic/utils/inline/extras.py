cd4d7cdca247d94e742928ffb17c6614a1ccfe62
KASHIBRAND <kashika6587@gmail.com>
2024-10-09 20:49:15 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+_qPchBV5z4VjMjM1"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+jcvUnNVqpNliMDE1"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
